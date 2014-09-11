fdbee25fc85e9a87f24c33e7185ae4918583b287
Sean Templeton <sean@pushup.com>
2014-09-11 15:43:01 -0500

    Added project less files

--- GruntFile.js
+++ GruntFile.js
@@ -13,6 +13,18 @@ module.exports = function(grunt) {
 		        files: {
 		            "css/plugins.css": "less/plugins.less"
 		        }
+			},
+			app: {
+				options: {
+		            paths: [
+		                'less/'
+		            ],
+		            cleancss: false,
+		            sourceMap: false
+		        },
+		        files: {
+		            "css/main.css": "less/main.less"
+		        }
 			}
 		},
 		uglify: {
@@ -30,10 +42,21 @@ module.exports = function(grunt) {
 		            ]
 		        }
 		    }
+		},
+		watch: {
+			app_less: {
+				files: ['less/**/*.less', '!less/plugins.less'],
+				tasks: 'less:app'
+			},
+			plugin_less: {
+				files: ['less/plugins.less'],
+				tasks: 'less:plugins'
+			}
 		}
 	});
 
 	// Load our npm grunt tasks
   	grunt.loadNpmTasks('grunt-contrib-less');
   	grunt.loadNpmTasks('grunt-contrib-uglify');
+  	grunt.loadNpmTasks('grunt-contrib-watch');
 }
